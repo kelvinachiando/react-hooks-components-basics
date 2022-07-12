1790bb9ddd2512a335f176261a3b671c7830540c
kelvinachiando <kelvinostin22gmail.com>
2022-07-12 21:26:29 +0300

    Done.

--- src/Article.js
+++ src/Article.js
@@ -2,7 +2,9 @@ import React from "react";
 
 function Article() {
   return (
-    <div>Dear Reader: Bjarne Stroustrup has the perfect lecture oration.</div>
+    <div>
+      Dear Reader: Bjarne Stroustrup has the perfect lecture oration.
+      </div>
   );
 }
 
